8913c10100b144879ec78bdf22559aca6f9dfb83
mcmatiss <160856820+mcmatiss@users.noreply.github.com>
2024-06-08 12:01:43 +0300

    Update Warehouse.php

--- app/Warehouse.php
+++ app/Warehouse.php
@@ -1,65 +1,137 @@
 <?php
+
 namespace App;
 
-use Carbon\Carbon;
-
-class Warehouse implements \JsonSerializable
+class Warehouse
 {
     private array $products = [];
     private array $log = [];
     private const STORAGE_PATH = 'data/';
+
     public function __construct()
     {
-        $products = file_get_contents(self::STORAGE_PATH . "storedProducts.json");
-        $products = json_decode($products);
+        if (file_exists(self::STORAGE_PATH . "logs.json")) {
+            $log = json_decode(file_get_contents("data/logs.json"));
+            foreach ($log as $oldEntry) {
+                $this->log[] = $oldEntry;
+            }
+        }
 
-        foreach ($products as $product)
-        {
-            $this->addProduct(new Product(
-                $product->id,
-                $product->name,
-                $product->createdAt,
-                $product->updatedAt,
-                $product->units
-            ));
+        if (file_exists(self::STORAGE_PATH . "storedProducts.json")) {
+            $products = file_get_contents(self::STORAGE_PATH . "storedProducts.json");
+            $products = json_decode($products);
+
+            foreach ($products as $product) {
+                $this->addProduct(new Product(
+                    $product->name,
+                    $product->units,
+                    $product->price,
+                    $product->expirationDate,
+                    $product->id,
+                    $product->createdAt,
+                    $product->updatedAt
+                ));
+            }
         }
     }
+
     public function products(): array
     {
         return $this->products;
     }
+
     public function addProduct(Product $product): void
     {
         $this->products[] = $product;
+
     }
+
     public function removeProduct(int $index): void
     {
         array_splice($this->products, $index, 1);
     }
+
     public function createLog(string $time, string $id, string $user, string $msg): void
     {
         $this->log[] = "[$time] product id: [$id] user: [$user] - $msg";
-    }
-    public function updateLogs(array $logs): void
-    {
-        foreach ($logs as $oldLog) {
-            $this->log[] = $oldLog;
-        }
-    }
-    public function getLogs(): array
-    {
-        return $this->log;
+
+        $this->updateLogs();
+
+        $this->save();
     }
 
     public function viewLogs(): void
     {
-        foreach ($this->log as $entry => $log)
-        {
+        foreach ($this->log as $entry => $log) {
             echo "[$entry] $log\n";
         }
     }
-    public function jsonSerialize()
+
+
+    public function createReport(): string
     {
-        return json_encode($this->products, JSON_PRETTY_PRINT);
+        return "\nTotal amount of products: " .
+            $this->totalAmountOfProducts() .
+            "\nTotal amount of units: " .
+            $this->totalAmountOfUnits() .
+            "\nTotal product value in EUR: " .
+            $this->totalProductValue() .
+            "\n";
+    }
+
+    public function saveReport(): void
+    {
+        file_put_contents(
+            self::STORAGE_PATH .
+            "report.json",
+            json_encode($this->createReport(),
+                JSON_PRETTY_PRINT
+            )
+        );
+    }
+
+    private function totalAmountOfProducts(): int
+    {
+        return count($this->products);
+    }
+
+    private function totalAmountOfUnits(): int
+    {
+        $sum = 0;
+        foreach ($this->products as $product) {
+            $sum += $product->getUnits();
+        }
+        return $sum;
+    }
+
+    private function totalProductValue(): float
+    {
+        $sum = 0;
+        foreach ($this->products as $product) {
+            $sum += $product->getPrice();
+        }
+        return $sum;
+    }
+
+    private function updateLogs(): void
+    {
+        file_put_contents(
+            self::STORAGE_PATH .
+            "logs.json",
+            json_encode($this->log,
+                JSON_PRETTY_PRINT
+            )
+        );
+    }
+
+    private function save(): void
+    {
+        file_put_contents(
+            self::STORAGE_PATH .
+            "storedProducts.json",
+            json_encode($this->products,
+                JSON_PRETTY_PRINT
+            )
+        );
     }
 }
