783fb2ecb4236e6ff897f075c526edd31266149b
mcmatiss <160856820+mcmatiss@users.noreply.github.com>
2024-06-08 12:01:17 +0300

    Update Product.php

--- app/Product.php
+++ app/Product.php
@@ -1,34 +1,37 @@
 <?php
+
 namespace App;
 
 use Carbon\Carbon;
+use JsonSerializable;
 
-class Product implements \JsonSerializable
+class Product implements JsonSerializable
 {
-    private string $id;
     private string $name;
+    private int $units;
+    private float $price;
+    private ?string $expirationDate;
+    private ?string $id;
     private Carbon $createdAt;
     private ?Carbon $updatedAt;
-    private int $units;
 
     public function __construct(
-        string $name,
-        int $units,
-        string $id = null,
-        Carbon $createdAt = null,
-        ?Carbon $updatedAt = null
+        string  $name,
+        int     $units,
+        float   $price,
+        ?string $expirationDate = null,
+        ?string $id = null,
+        ?string $createdAt = null,
+        ?string $updatedAt = null
     )
     {
-        $this->id = $id ? self::getId() : $this->generateUuid();
         $this->name = $name;
-        $this->createdAt = $createdAt ? new Carbon($createdAt) : Carbon::now('UTC');
-        $this->updatedAt = $updatedAt ? new Carbon($updatedAt) : null;
         $this->units = $units;
-    }
-
-    public function getId(): string
-    {
-        return $this->id;
+        $this->price = $price;
+        $this->expirationDate = $expirationDate;
+        $this->id = $id ?: $this->generateUuid();
+        $this->createdAt = $createdAt ? Carbon::parse($createdAt) : Carbon::now('UTC');
+        $this->updatedAt = $updatedAt ? Carbon::parse($updatedAt) : null;
     }
 
     public function getName(): string
@@ -39,6 +42,56 @@ class Product implements \JsonSerializable
     public function setName(string $name): void
     {
         $this->name = $name;
+
+        $this->update();
+    }
+
+    public function getUnits(): int
+    {
+        return $this->units;
+    }
+
+    public function setUnits(int $units): void
+    {
+        $this->units = $units;
+
+        $this->update();
+    }
+
+    public function withdrawUnits(int $amount): void
+    {
+        $this->units -= $amount;
+
+        $this->update();
+    }
+
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): void
+    {
+        $this->price = $price;
+
+        $this->update();
+    }
+
+    public function getExpirationDate(): ?string
+    {
+        return $this->expirationDate;
+    }
+
+    public function setExpirationDate(string $expirationDate): void
+    {
+        $this->expirationDate = $expirationDate;
+
+        $this->update();
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
     }
 
     public function getCreatedAt(): Carbon
@@ -51,43 +104,30 @@ class Product implements \JsonSerializable
         return $this->updatedAt ? $this->updatedAt->setTimezone('Europe/Riga') : null;
     }
 
-    public function update(): void
+    public function jsonSerialize(): array
+    {
+        return [
+            'name' => $this->name,
+            'units' => $this->units,
+            'price' => $this->price,
+            'expirationDate' => $this->expirationDate,
+            'id' => $this->id,
+            'createdAt' => $this->createdAt,
+            'updatedAt' => $this->updatedAt
+        ];
+    }
+
+    private function update(): void
     {
         $this->updatedAt = Carbon::now('UTC');
     }
 
-    public function getUnits(): int
-    {
-        return $this->units;
-    }
-
-    public function setUnits(int $units): void
-    {
-        $this->units = $units;
-    }
-
-    public function withdrawUnits(int $amount): void
-    {
-        $this->units -= $amount;
-    }
-
-    public function generateUuid(): string
+    private function generateUuid(): string
     {
         return json_decode(
             file_get_contents(
                 'https://www.uuidtools.com/api/generate/v4'
             )
         )[0];
-    }
-
-    public function jsonSerialize(): array
-    {
-        return [
-            'id' => $this->id,
-            'name' => $this->name,
-            'createdAt' => $this->createdAt,
-            'updatedAt' => $this->updatedAt,
-            'units' => $this->units
-        ];
     }
 }
